eed5e4f25ed0aa76a4fdf8da5e8c24fbb603bc81
Basant <basantanand99@gmail.com>
2024-07-24 14:03:38 +0530

    Basant

--- app/components/CardList.js
+++ app/components/CardList.js
@@ -35,7 +35,7 @@ const CardList = () => {
       infinite: true,
       speed: 500,
       slidesToShow: 4,
-      slidesToScroll: 3,
+      slidesToScroll: 1,
         nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />
       };
