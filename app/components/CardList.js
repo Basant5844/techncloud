11dd7b06137529cf3369e06342181dde040b02d9
Basant <basantanand99@gmail.com>
2024-07-24 13:59:38 +0530

    Basant

--- app/components/CardList.js
+++ app/components/CardList.js
@@ -31,7 +31,7 @@ function SamplePrevArrow(props) {
 const CardList = () => {
 
     const settings = {
-      dots: true,
+      dots: false,
       infinite: true,
       speed: 500,
       slidesToShow: 4,
